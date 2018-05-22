997b1818e3c3c64e90b0aeb715b03de4ebe9393d
Alec Glassford <glassford@cs.stanford.edu>
2018-05-21 18:10:14 -0700

    First attempt at Markdown parse

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,20 @@ export default [
     output: {
       file: 'build/main.js',
       format: 'iife',
-    }
+    },
   },
   {
     input: 'src/inject.js',
     output: {
       file: 'build/inject.js',
       format: 'iife',
-    }
-  },{
+    },
+  },
+  {
     input: 'src/injectception.js',
     output: {
       file: 'build/injectception.js',
       format: 'iife',
-    }
-  }
+    },
+  },
 ];
--- src/injectception.js
+++ src/injectception.js
@@ -1,41 +1,87 @@
 /* eslint-env browser */
 
-
 const getData = async function getData(postId, nonce) {
   const endpoint = `https://www.seattletimes.com/wp-json/wp/v2/posts/${postId}?context=edit&_wpnonce=${nonce}`;
   const res = await fetch(endpoint, { credentials: 'include' });
   return res.json();
 };
 
-const parse = function parse(soFar, node) {
-  const children = Array.from(node.childNodes);
-  if (children.length === 0) return soFar + node.textContent;
+const openTag = function openTag(node) {
+  const endOpen = node.outerHTML.indexOf('>');
+  return node.outerHTML.slice(0, endOpen + 1);
+};
 
-  const childContent = children.reduce(parse, '');
-  switch (node.nodeName) {
-    case 'H1': {
-      return `${soFar}# ${childContent.trim()}`;
-    }
-    case 'H2': {
-      return `${soFar}## ${childContent.trim()}`;
-    }
-    case 'H3': {
-      return `${soFar}### ${childContent.trim()}`;
-    }
-    case 'H4': {
-      return `${soFar}#### ${childContent.trim()}`;
-    }
-    default: {
-      return `${soFar}${childContent}`;
-    }
+const closeTag = function closeTag(node) {
+  // Not every node has a close tag
+  if (node.innerHTML === '') return '';
+
+  const startClose = node.outerHTML.lastIndexOf('<');
+  return node.outerHTML.slice(startClose);
+};
+
+const removeShortcodes = function removeShortcodes(str) {
+  return str.replace(/\[.*?\]/g, '');
+};
+
+const removeLinebreaks = function removeLinebreaks(str) {
+  return str.replace(/\r?\n|\r/g, ' '); // https://stackoverflow.com/a/10805292
+};
+
+const cleanWhitespace = function cleanWhitespace(str) {
+  return str.split('\n')
+    .map(text => text.trim())
+    .filter(text => text.length > 0)
+    .join('\n\n');
+};
+
+const format = {
+  IMG: n => `![${n.getAttribute('alt') || 'TK'}](${n.getAttribute('src')})`,
+  BODY: (n, cc) => cc,
+  H1: (n, cc) => `# ${removeLinebreaks(cc)}`,
+  H2: (n, cc) => `## ${removeLinebreaks(cc)}`,
+  H3: (n, cc) => `### ${removeLinebreaks(cc)}`,
+  H4: (n, cc) => `#### ${removeLinebreaks(cc)}`,
+  A: (n, cc) => `[${cc}](${n.getAttribute('href') || 'TK'})`,
+  I: (n, cc) => `*${cc}*`,
+  EM: (n, cc) => `*${cc}*`,
+  B: (n, cc) => `**${cc}**`,
+  STRONG: (n, cc) => `**${cc}**`,
+  // Wordpress API doesn't usually give paragraphs
+  // usually just split by linebreaks like Markdown
+  // But I saw at least one case where it did have <p> nodes (Associated Press import)
+  // Only putting one line break on each side,
+  // with the assumption there will be more <p>s on either side (generally)
+  P: (n, cc) => `\n\n${cc}`,
+};
+
+const parse = function parse(soFar, node) {
+  // Case 1: text node leaf
+  if (node.nodeName === '#text') return soFar + node.textContent;
+
+  const children = Array.from(node.childNodes);
+  if (children.length === 0) {
+    // Case 2: non-text node leaf + we have a formatter (e.g. <img>)
+    if (format[node.nodeName]) return soFar + format[node.nodeName](node);
+
+    // Case 3: non-text node leaf + we don't have a formatter (e.g. <input>)
+    return soFar + node.outerHTML;
   }
+
+  // Parse children --> html string
+  const childContent = children.reduce(parse, '');
+
+  // Case 4: non-leaf + we have a formatter for it
+  if (format[node.nodeName]) return soFar + format[node.nodeName](node, childContent);
+
+  // Case 5: non-leaf + we don't have a formatter for it
+  return soFar + openTag(node) + childContent + closeTag(node);
 };
 
 const toMarkdown = function toMarkdown(input) {
   const doc = document.implementation.createHTMLDocument();
-  doc.body.innerHTML = input;
-  window.debugdoc = doc;
-  return parse('', doc.body);
+  doc.body.innerHTML = removeShortcodes(input);
+  const result = parse('', doc.body);
+  return cleanWhitespace(result);
 };
 
 const main = async function main() {
